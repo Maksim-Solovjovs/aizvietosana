0ad714ed28c957da36c2f5264d335812d348d3b5
Max <77140683+Maksim-Solovjovs@users.noreply.github.com>
2022-01-18 07:58:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+teksts = input("ievadiet tekstu: ")
+def replaceTwos(teksts):
+  if teksts.count("2")>0:
+    teksts = teksts.replace("2","divi")
+    print(teksts)
+  else:
+    teksts = "nekas netika aizvietots"
+    print(teksts)
+  return teksts
+replaceTwos(teksts)
